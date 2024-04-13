a52d1e2d5a8b65949a3b6720857db0d5380aa403
Sameh Mohamed <126187468+samehmohamedka7raba@users.noreply.github.com>
2024-04-13 14:08:54 +0200

    methods

--- Bank-System-Phase-2/Admin.h
+++ Bank-System-Phase-2/Admin.h
@@ -1,11 +1,52 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector>
 #include"Employee.h"
 #include "Validation.h"
 using namespace std;
 class Admin : public Employee {
+private:
+   vector <Employee> employees;
 public:
     //Constructors:
     Admin(int id, string name, string password, double salary) : Employee(id, name, password, salary) {}
+    //methods
+  
+    void addEmployee(Employee& employee) {
+        employees.push_back (employee);
+    }
+
+    Employee* SearchEmployee(int id) {
+        for (auto& employee : employees) {
+            if (employee.getID() == id) {
+                return& employee;
+            }
+        }
+        return nullptr;
+    }
+
+    void listEmployee() {
+        cout << "Employee:\n";
+        for (auto& employee : employees) {
+            cout << "ID: " << employee.getID() << ", Name: " << employee.getName() << ", Balance: " << employee.getSalary() << endl;
+        }
+    }
+
+    void editEmployee(int id, string name, string password, double salary) {
+        for (auto& employee : employees) {
+            if (employee.getID() == id) {
+                employee.setName(name);
+                employee.setPassword(password);
+                employee.setSalary(salary);
+                cout << "Employee information updated successfully.\n";
+            }
+            else
+            {
+                cout << "Employee not found\n";
+            }
+        }
+    }
+
+
 };
--- Bank-System-Phase-2/Employee.h
+++ Bank-System-Phase-2/Employee.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector>
 #include "Person.h"
 #include "Client.h"
 #include "Validation.h"
@@ -9,6 +10,7 @@ class Employee : public Person {
 protected:
     //Attributs:
     double salary;
+    vector <Client> Clients;
 public:
     //Consttructors:
     Employee(int id, string name, string password, double salary) : Person(id, name, password), salary(0) {
@@ -30,4 +32,41 @@ public:
         Person::display();
         cout << " Salary : " << getSalary() << "$" << endl;
     }
+
+
+    void addClient(Client& Client) {
+        Clients.push_back (Client);
+    }
+
+    Client* SearchClient(int id) {
+        for (auto& Client : Clients) {
+            if (Client.getID() == id) {
+                return& Client;
+            }
+        }
+        return nullptr;
+    }
+
+    void listClients() {
+        cout << "Clients:\n";
+        for (auto& Client : Clients) {
+            cout << "ID: " << Client.getID() << ", Name: " << Client.getName() << ", Balance: " << Client.getBalance() << endl;
+        }
+    }
+
+    void editClient(int id, string name, string password, double balance) {
+        for (auto& Client : Clients) {
+            if (Client.getID() == id) {
+                Client.setName(name);
+                Client.setPassword(password);
+                Client.setBalance(balance);
+                cout << "Client information updated successfully.\n";
+            }
+            else
+            {
+                cout << "Client not found\n";
+            }
+        }
+    }
+
 };
